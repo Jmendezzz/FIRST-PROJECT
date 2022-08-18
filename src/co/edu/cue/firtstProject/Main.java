431ed69c3135ac4167861c0b1cb9f685ca57ab21
Juan Gerardo <jmendez1025@cue.edu.co>
2022-08-18 14:42:35 -0500

    herencia

--- src/co/edu/cue/firtstProject/Main.java
+++ src/co/edu/cue/firtstProject/Main.java
@@ -2,7 +2,7 @@ package co.edu.cue.firtstProject;
 
 public class Main {
     public static void main(String[] args) {
-        Calculadora calculadora = new Calculadora(3,2);
+        Calculadora calculadora = new Calculadora(3,5);
         calculadora.sumar();
         System.out.println("El resultado de la resta es: "+calculadora.restar(8,2));
     }
